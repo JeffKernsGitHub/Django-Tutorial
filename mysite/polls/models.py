ce0e265c0a0532cb0d33aa9d69199a60186c195b
Jeff Kerns <jbkernsjob@gmail.com>
2018-03-04 13:30:45 -0500

    Part 3 commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+
 
 # Create your models here.
 # the python shell did not do tabs very well, had to replace with spaces
@@ -12,6 +14,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def detail(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, 'polls/detail.html', {'question': question})    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
