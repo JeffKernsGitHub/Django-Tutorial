75af262cc289941969c2364298fe6b8fb4dbb349
Jeff Kerns <jbkernsjob@gmail.com>
2018-03-05 19:12:12 -0500

    Advanced Commit

---
+++ django-polls/polls/models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+
+
+# Create your models here.
+# the python shell did not do tabs very well, had to replace with spaces
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'    
+    def detail(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, 'polls/detail.html', {'question': question})    
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+    
